a3d5535c4d875d9a446a2ed2bf337fbec1b32a14
Habib <129363789+habibqureshi488@users.noreply.github.com>
2024-06-23 16:33:45 +0500

    Update app.js

--- PWA/js/app.js
+++ PWA/js/app.js
@@ -9,6 +9,18 @@ const coffees = [
   { name: "Inventore", image: "Cars/7.jpg" },
   { name: "Dodge Challenger", image: "Cars/8.jpg" },
   { name: "Lamborghani", image: "Cars/9.jpg" },
+  { name: "Range Rover", image: "Cars/10.jpg" },
+  { name: "Buggati Chiron", image: "Cars/11.jpg" },
+  { name: "BMW", image: "Cars/12.jpg" },
+  { name: "Rolls Royces", image: "Cars/13.jpg" },
+  { name: "LEXUS", image: "Cars/14.jpg" },
+  { name: "Lamborghani URUS", image: "Cars/15.jpg" },
+  { name: "BMW i7", image: "Cars/16.jpg" },
+  { name: "Bently", image: "Cars/17.jpg" },
+  { name: "Audi", image: "Cars/18.jpg" },
+  { name: "Buggati bolide", image: "Cars/19.jpg" },
+  { name: "G-Wagon", image: "Cars/20.jpg" },
+  // { name: "LEXUS", image: "Cars/14.jpg" },
 ]
 const showCoffees = () => {
     let output = ""
