96aa807b4112bd5f05414a1c5808da0ce1d191de
Habib <129363789+habibqureshi488@users.noreply.github.com>
2024-06-23 16:17:22 +0500

    Update serviceWorker.js

--- PWA/serviceWorker.js
+++ PWA/serviceWorker.js
@@ -30,11 +30,13 @@ self.addEventListener("fetch", fetchEvent => {
       })
     )
   })
-  if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
         .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
+        .then(res => console.log("service worker registered"),
+        alert("service worker registered"))
+        .catch(err => console.log("service worker not registered",
+        alert("service worker not registered") , err))
     })
   }
